f1690661028e50b6ff90b11d8db8d7465822b488
he liu <liuhe9@qq.com>
2019-09-23 19:26:29 +0800

    config

--- deploy.php
+++ deploy.php
@@ -12,9 +12,6 @@ set('repository', 'git@github.com:liuhe9/wuliu-api.git');
 // 保存最近5个版本
 set('keep_releases', 5);
 
-// 不用sudo
-set('writable_use_sudo', false);
-
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true);
 
@@ -50,24 +47,24 @@ task('artisan:config:cache', function () {})->desc('artisan:config:cache nothing
 task('artisan:optimize', function () {})->desc('artisan:optimize nothing todo');
 
 
-// nginx
+// nginx文件
+desc('Upload nginx file');
 task('nginx_conf', function () {
-    run('cd {{release_path}} && cp -rf nginx/api.conf /etc/nginx/sites-enabled/');
+    upload('nginx/api.conf', '/etc/nginx/sites-enabled/');
+    upload('nginx/cert/', '/etc/nginx/cert/');
     run('sudo service nginx reload');
 });
 
+// 将本地的 .env 文件上传到代码目录的 .env
 desc('Upload .env file');
 task('env:upload', function () {
-    // 将本地的 .env 文件上传到代码目录的 .env
-    upload('.env.prod', '{{release_path}}/');
+    upload('./.env.prod', '{{release_path}}/.env');
 });
 
-// 定义一个后置钩子，在 deploy:shared 之后执行 env:upload 任务
 after('deploy:shared', 'env:upload');
 
 after('deploy:failed', 'deploy:unlock');
-
 before('deploy:symlink', 'artisan:migrate');
-before('deploy:symlink', 'artisan:db:seed');
+// before('deploy:symlink', 'artisan:db:seed');
 after('deploy:symlink', 'opcache_reset');
 after('deploy:symlink', 'nginx_conf');
