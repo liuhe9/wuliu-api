f48211a661914dde69406d1ad8f6477bfc12529a
he liu <liuhe9@qq.com>
2019-09-23 15:50:23 +0800

    nginx

--- deploy.php
+++ deploy.php
@@ -4,31 +4,54 @@ namespace Deployer;
 require 'recipe/laravel.php';
 
 // Project name
-set('application', 'my_project');
+set('application', '物流api');
 
 // Project repository
-set('repository', 'git@domain.com:username/repository.git');
+set('repository', 'git@github.com:liuhe9/wuliu-api.git');
+
+// 保存最近5个版本
+set('keep_releases', 5);
+
+// 不用sudo
+set('writable_use_sudo', false);
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+set('git_tty', true);
 
-// Shared files/dirs between deploys 
+// Shared files/dirs between deploys
 add('shared_files', []);
 add('shared_dirs', []);
 
-// Writable dirs by web server 
+// Writable dirs by web server
 add('writable_dirs', []);
 
 
 // Hosts
+host('liuhetx')
+    ->stage('dev')
+    ->user('ubuntu')
+    ->port(22)
+    ->set('deploy_path', '/data/deploy/wuliu-api')
+    ->forwardAgent(true)
+    ->multiplexing(true)
+    ->addSshOption('UserKnownHostsFile', '/dev/null')
+    ->addSshOption('StrictHostKeyChecking', 'no');
 
-host('project.com')
-    ->set('deploy_path', '~/{{application}}');    
-    
-// Tasks
+// 自定义任务：重置 opcache 缓存
+task('opcache_reset', function () {
+    run('{{bin/php}} -r \'opcache_reset();\'');
+});
 
-task('build', function () {
-    run('cd {{release_path}} && build');
+// Lumen重置任务
+task('artisan:storage:link', function () {})->desc('artisan:storage:link nothing todo');
+task('artisan:view:cache', function () {})->desc('artisan:view:cache nothing todo');
+task('artisan:config:cache', function () {})->desc('artisan:config:cache nothing todo');
+task('artisan:optimize', function () {})->desc('artisan:optimize nothing todo');
+
+
+// nginx
+task('nginx_conf', function () {
+    run('cd {{release_path}} && cp nginx/api.conf /etc/nginx/sites-enabled/');
 });
 
 // [Optional] if deploy fails automatically unlock.
@@ -37,4 +60,7 @@ after('deploy:failed', 'deploy:unlock');
 // Migrate database before symlink new release.
 
 before('deploy:symlink', 'artisan:migrate');
+after('deploy:symlink', 'opcache_reset');
 
+// after('deploy:update_code', 'artisan:migrate');
+after('deploy:update_code', 'nginx_conf');
