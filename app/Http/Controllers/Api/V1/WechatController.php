bf714fc490dd91c95c01bc6a70694571c7e0e941
he liu <liuhe9@qq.com>
2019-09-24 19:01:58 +0800

    gitignore

--- app/Http/Controllers/Api/V1/WechatController.php
+++ app/Http/Controllers/Api/V1/WechatController.php
@@ -9,7 +9,7 @@ class WechatController extends BaseController
 {
     public function serve()
     {
-        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+        Log::info('request arrived.');
 
         $app = app('wechat.mini_program');
         $app->server->push(function($message){
